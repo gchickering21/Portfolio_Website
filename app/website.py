4c2c98f9b89bb5c8747a44199cb0375bb51e1804
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2021-02-04 18:33:28 -0500

    Add files via upload

---
+++ app/website.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, Flask, g, jsonify, render_template, request, Response, redirect, url_for, send_from_directory
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+
+# Configure application
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/portfolio/')
+def portfolio():
+    return render_template('portfolio.html')
+
+if __name__ == '__main__':
+    app.run()
